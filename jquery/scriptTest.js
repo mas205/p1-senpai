a513f5783a7ffe560839a14e1a1ffe3a2415ceaa
martin.hargain <maritnhargain@gmail.com>
2019-06-04 21:55:52 -0300

    jquery small change

--- jquery/scriptTest.js
+++ jquery/scriptTest.js
@@ -3,9 +3,19 @@ $(document).ready(main);
 const main = () => {
     alert('prueba');
     //$('#buttonID').click(someFunction);
+    $('body').click(quit);
+    $('#openModalExample').click(openModal)
 };
 
 
 const someFunction = () => {
     alert("Esto deberia funcionar");
+}
+
+const quit = (event) => {
+
+}
+
+const openModal = (event) => {
+    event.stopPropagation();
 }
